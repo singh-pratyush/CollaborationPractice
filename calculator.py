6dffcacf55df44ae606e63151197aa65cc9790d3
il798li <92280179+il798li@users.noreply.github.com>
2025-07-18 17:28:41 -0700

    reset files for lecture

--- calculator.py
+++ calculator.py
@@ -1,60 +1,21 @@
-from __future__ import annotations
-
 class Calculator:
     def multiply(factor1: float, factor2: float) -> float:
-        """
-        Multiplies two numbers.
-
-        Args:
-            factor1 (float): The first factor to multiply.
-            factor2 (float): The second factor to multiply.
-
-        Returns:
-            float: The product of the two factors.
-        """
-        return factor1 * factor2
+        pass
 
     def divide(dividend: float, divisor: float) -> float:
-        """
-        Divides two numbers.
-
-        Args:
-            dividend (float): The number to divide.
-            divisor (float): The number to divide by.
-
-        Returns:
-            float: The result of the division.
-        """
+        pass
     
     def add(number1: float, number2: float) -> float:
-        """
-        Adds two numbers. This one has been done as an example.
-
-        Args:
-            number1 (float): The first number to add.
-            number2 (float): The second number to add.
-
-        Returns:
-            float: The sum of the two numbers.
-        """
-        return number1 + number2
+        pass
     
     def subtract(minuend: float, subtrahend: float) -> float:
-        """
-        Subtracts two numbers.
-
-        Args:
-            minuend (float): The number to subtract from.
-            subtrahend (float): The number to subtract.
-
-        Returns:
-            float: The difference between the two numbers.
-        """
+        pass
     
     def power(base: float, exponent: int) -> float:
-        """
-        Raises a number to a power. Challenge: Try to use `multiply` to implement this!
+        pass
+    
 
+<<<<<<< Updated upstream
         Args:
             base (float): The number to raise.
             exponent (float): The power to raise the number to.
@@ -64,6 +25,8 @@ class Calculator:
         """
 
 # Don't 
+=======
+>>>>>>> Stashed changes
 assert Calculator.multiply(2, 3) == 6
 assert Calculator.divide(6, 2) == 3
 assert Calculator.add(2, 3) == 5
